d5bfb9fead7faf85bbbd4affe712d95a50550b98
Travis Martin <travisemerymartin@gmail.com>
2022-05-22 14:00:50 -0400

    Add express.js server for file watching

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import Express from "express";
+import { Router } from "express";
+
+const APP = Express();
+const ROUTER = Router();
+const port = process.env.PORT || 3000;
+
+ROUTER.get("/", async (req, res) => {
+	res.status(200).json({ message: "Server is running." });
+});
+
+APP.use("/", ROUTER);
+
+APP.listen(port, () => {
+	console.log(`Server listening on port ${port}.`);
+});
