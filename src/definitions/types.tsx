69b2002549222aa3a7dd27c27718f2f210414af9
Travis Martin <travisemerymartin@gmail.com>
2022-09-15 14:52:58 -0400

    Add types for coords and dims

--- src/definitions/types.tsx
+++ src/definitions/types.tsx
@@ -1,5 +1,20 @@
 import { ReactNode } from "react";
 
+export function noop(): void {};
+
 export interface Props {
 	children?: ReactNode;
+}
+
+export interface Coordinates { 
+	x: number;
+	y: number;
+	z?: number;
+	h?: number;
+};
+
+export interface Dimensions {
+	rows: number;
+	cols: number;
+	depth: number;
 }
