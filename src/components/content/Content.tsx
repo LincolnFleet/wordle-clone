40294e4331083f2c46c60093a402963f0d0719cd
Travis Martin <travisemerymartin@gmail.com>
2022-09-19 17:53:48 -0400

    Refactor <Content> and <Row>
    - remove <Row>, move logic and state to parent.
    - convert back to useReducer and map string graph to lmnts

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,53 +1,106 @@
-import { FC, ReactElement, useState, useEffect, useMemo } from "react";
+import {
+	useState,
+	useEffect,
+	useReducer,
+	SyntheticEvent,
+	KeyboardEventHandler,
+	ReactNode,
+} from "react";
 import "./content.css";
-import { Props, Dimensions } from "../../definitions/types";
+import { Coordinates, Props } from "../../definitions/types";
+import { noop } from "../../definitions/modules";
+import { charGraphReducer, CELL_SET, CELL_UNSET } from "./char_graph_reducer";
+import Toolbar from "../toolbar/Toolbar";
+
 
 interface ContentProps extends Props {
-	target: string;
+	secretWord: string;
 	attempts: number;
 }
 
-function makeGraph(
-	{ rowCount, colCount }: Dimensions,
-	guesses: string[],
-	defaultChar: string = ""
-): string[][] {
-	const graph = Array.from({ length: rowCount }, () =>
-		Array.from({ length: colCount }, () => "")
+function Content({ secretWord, attempts }: ContentProps): ReactNode {
+	const [charGraph, dispatchCharGraph]: [{ [index: string]: any }, Function] = useReducer(
+		charGraphReducer,
+		Array.from({ length: attempts }, () =>Array.from({ length: secretWord.length }, () => "")),
 	);
 
-	return graph.map((rows, idxY) => rows.map((cell, idxX) => graph[idxY][idxX] || defaultChar));
-}
+    const handleOnKeyDown: KeyboardEventHandler = (event: SyntheticEvent, coords: Coordinates) => {
+        event.preventDefault();
+        console.log({ event });
+		const { key, target, stopPropagation } = event;
+        const {y, x} = coords;
 
-const Content: FC<ContentProps> = ({ target, attempts }: ContentProps) => {
-	const [guesses, setGuesses]: [string[], Function] = useState([""]);
-	const targetClean: string = useMemo(() => target.trim(), [target]);
+        switch (key) {
+            case "Backspace":
+                stopPropagation();
+                dispatchCharGraph({type: CELL_UNSET, data: coords})
+                // TODO: focus (idxX - 1) or preceding sibling
+            break;
 
-	// init, unmount
-	useEffect(() => {}, []);
+/* hoping form can handle navigation on its own, otherwise will implement
+            case "ArrowUp":
+                // TODO: drill cb to focus (idxY - 1) || allow to bubble and handle in content
+            case "ArrowDown":
+                // TODO: drill cb to focus (idxY + 1) || allow to bubble and handle in content
+            break;
 
-	// make data graph and fill with html cells
-	const charGraph = useMemo(
-		() => makeGraph({ rowCount: attempts, colCount: targetClean.length }, guesses),
-		[targetClean, attempts, guesses]
-	);
+            case "ArrowLeft":
+                stopPropagation();
+                const { nextSibling } = ref.current;
+                if (nextSibling) {
+                    nextSibling.focus();
+                }
+                // TODO: focus (idxX - 1) or preceding sibling
+            break;
 
-	console.log({ charGraph });
+            case "ArrowRight":
+                stopPropagation();
+                // const {prevSibling} = ref.current;
+                if (prevSibling) {
+                    prevSibling.focus();
+                }
+            break;
+
+            case "Enter":
+                // TODO: submit row/form
+            break;
+*/
+            default:
+                stopPropagation();
+                if (/^Key[A-Za-z]{1}$/g.test(key)) {  // ensure keystroke is a letter
+                    dispatchCharGraph({type: CELL_SET, data: coords})
+                } else {
+                    // box shake animation?
+                }
+        }
+
+	const handleOnNavKeyDown: KeyboardEventHandler = () => {};
+	const rowProps = {
+		onKeyDown: handleOnNavKeyDown,
+		handleOnNavKeyDown,
+		secretWord,
+		coords: { y },
+	};
 
 	return (
 		<main data-testid="Content">
-			<div className="toolbar">~~~Toolbar~~~</div>
 			<div className="grid">
-				{charGraph.map((row) => (
-					<div className="row">
-						{row.map((char) => (
-							<div className="cell">{char}</div>
-						))}
-					</div>
-				))}
+				{/* ranges in js are dumb AF */}
+				{charGraph.map((row: string[], y: number) => {
+					<form className={`attempt ${y}`}>
+						{row.map((char, x) => {
+							return (
+								<input className={`cell ${y + "," + x}`} onKeyDown={handleOnKeyDown}>
+									{char}
+								</input>
+							);
+						})}
+					</form>;
+				})}
 			</div>
+			<Toolbar />
 		</main>
 	);
-};
+}
 
 export default Content;
--- src/components/content/char_graph_reducer.tsx
+++ src/components/content/char_graph_reducer.tsx
@@ -1,29 +1,30 @@
-import { ReactNode, RefCallback } from "react";
+import React from "react";
 import { ReducerAction } from "../../definitions/types";
 
 export const CELL_SET = "CELL_SET";
 export const CELL_UNSET = "CELL_UNSET";
 
-export function refGraphReducer(
-	state: readonly RefCallback<ReactNode>[][],
-	action: ReducerAction
-) {
+export function charGraphReducer(state: readonly string[][], action: ReducerAction) {
 	const { type, data, pipe } = action;
 	let newState = [...state];
 
+	const {
+		coords: { x, y },
+		value,
+	} = data;
+
 	switch (type) {
 		case CELL_SET:
-			const { y, x, value } = data;
-			if (newState[y][x]) {
-				let targetCell = newState[y][x].current;
-				targetCell.innerHTML = value;
+			if (!newState[y][x]) {
+				newState[y][x] = value;
 			} else {
 				console.error(`Failed CELL_SET with: \ndata: ${data}`);
 			}
 			return newState;
 
 		case CELL_UNSET:
-			return state;
+			newState[y][x] = "";
+			return newState;
 
 		default:
 			return state;
